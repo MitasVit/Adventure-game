12efebe7acc1d2029c779ffa6e9e63142a06c725
Pejsek34 <david.splichal07@gmail.com>
2022-03-29 17:45:36 +0200

    Update Resource.h

--- Map editor/Resource.h
+++ Map editor/Resource.h
@@ -1,38 +1,36 @@
-#pragma once
 //{{NO_DEPENDENCIES}}
-// Soubor k zahrnutí vygenerovaný pomocí Microsoft Visual C++
-// Používáno Game1.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_GAME1_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_GAME1			107
-#define IDI_SMALL				108
-#define IDC_GAME1			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-
-
-#define IDM_NEW 256
-#define IDM_SAVE 255
-#define IDM_OPEN 254
-
-
-#endif
-// Další výchozí hodnoty pro nové objekty
+// Microsoft Visual C++ vygeneroval soubor zahrnutí.
+// Použito Resource.rc
 //
+#define IDC_MYICON                      2
+#define IDD_GAME1_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_GAME1                       107
+#define IDI_SMALL                       108
+#define IDC_GAME1                       109
+#define IDR_MAINFRAME                   128
+#define IDM_OPEN                        254
+#define IDM_SAVE                        255
+#define IDM_NEW                         256
+#define IDC_CHECK1                      1000
+#define IDC_COMBO1                      1001
+#define IDC_LIST1                       1002
+#define IDC_BUTTON1                     1003
+#define IDC_CANCEL                      1003
+#define IDD_EDITCHOOSE                  1004
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
